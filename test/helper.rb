6a0e68d6fd90cd7b071e7fad22107e3e566667b1
Florian Dejonckheere <florian@floriandejonckheere.be>
2024-09-16 11:15:49 +0200

    Fix inspect format for Ruby >= 3.4.0

--- test/helper.rb
+++ test/helper.rb
@@ -44,6 +44,14 @@ module ::Kernel
   def monotonic_clock
     ::Process.clock_gettime(::Process::CLOCK_MONOTONIC)
   end
+
+  def inspect_method_name_for(klass_name, method_name)
+    if RUBY_VERSION < '3.4.0'
+      "`#{method_name}'"
+    else
+      "'#{klass_name}##{method_name}'"
+    end
+  end
 end
 
 class Minitest::Test
